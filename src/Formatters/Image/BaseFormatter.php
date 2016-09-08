517be6b638a3f0d633f71be50e56744268a6e0aa
Alexey Sejnov <alexey.sejnov@solbeg.com>
2016-09-08 15:39:02 +0300

    Removed intervention/image package from required.
    Signed-off-by: Alexey Sejnov <alexey.sejnov@solbeg.com>

--- src/Formatters/Image/BaseFormatter.php
+++ src/Formatters/Image/BaseFormatter.php
@@ -9,23 +9,4 @@ namespace Bicycle\FilesManager\Formatters\Image;
  */
 abstract class BaseFormatter extends \Bicycle\FilesManager\Formatters\AbstractFormatter
 {
-    const DRIVER_GD2 = 'gd2';
-    const DRIVER_IMAGICK = 'imagick';
-    const DRIVER_GMAGICK = 'gmagick';
-
-    /**
-     * @var array priority of imagine drivers.
-     * If `$driver` priority is null then drivers in this priority will be choose to use.
-     */
-    public static $driversPriority = [
-        self::DRIVER_GMAGICK,
-        self::DRIVER_IMAGICK,
-        self::DRIVER_GD2,
-    ];
-
-    /**
-     * @var string|null imagine driver that should be used.
-     * Null is meaning the driver will be detected autovatically.
-     */
-    protected $driver;
 }
